ddf70f18be3a6266652ff02d99477eed7831d777
Bharat <bharat.gmail.com>
2024-03-21 10:50:51 +0530

    java code changes

--- src/main/java/com/example/demo/Demo1Application.java
+++ src/main/java/com/example/demo/Demo1Application.java
@@ -5,8 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.ApplicationContext;
 
-@SpringBootApplication
-@EnableDiscoveryClient
+@SpringBootApplication 
 public class Demo1Application {
 
 	public static void main(String[] args) {
